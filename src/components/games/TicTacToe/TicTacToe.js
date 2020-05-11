ae33b13a399f09f0d027f4506cff7b9b605f008e
valuados <valuados@gmail.com>
2020-05-11 12:33:36 +0300

    finish history part

--- src/components/games/TicTacToe/TicTacToe.js
+++ src/components/games/TicTacToe/TicTacToe.js
@@ -55,12 +55,13 @@ class TicTacToe extends Component {
       history: [{
         squares: Array(9).fill(null)
       }],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -71,14 +72,33 @@ class TicTacToe extends Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
+    });
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) ===0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to the beginning';
+      return (
+        <li key={move}>
+          <Button size="lg" variant="light" onClick={() =>this.jumpTo(move)}>{desc}</Button>
+        </li>
+      )
+    });
 
     let status;
     if (winner) {
@@ -99,7 +119,7 @@ class TicTacToe extends Component {
             </div>
             <div className="game-info">
               <div>{status}</div>
-              <ol>{/* TODO */}</ol>
+              <ol>{moves}</ol>
             </div>
           </div>
         </Container>
